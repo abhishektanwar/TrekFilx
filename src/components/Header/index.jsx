72ef3ae6f47237d1f5e668a4119b223f5a0c1a50
=abhishektanwar <abhishektanwar084@gmail.com>
2022-04-04 03:03:12 +0530

    Header component code cleanup

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,6 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../Contexts/AuthDialogContext";
 import { useModal } from "../../Contexts/ModalContext";
-import { usePlaylist } from "../../Contexts/PlaylistContext";
-import BadgeIconButton from "../Buttons/BadgeIconButton";
 import Button from "../Buttons/Button";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
@@ -11,16 +9,11 @@ import SearchBar from "./SearchBar";
 const Header = () => {
   const { setAuthType, logoutHandler, user,authType } = useAuth();
   const { showModal } = useModal();
-  const {setShowPlaylistCreationModal} = usePlaylist()
   const handleLoginBtnClick = () => {
     showModal();
     setAuthType("login");
   };
 
-  const showPlaylist = () => {
-    setShowPlaylistCreationModal(true)
-    showModal()
-  }
   return (
     <>
       <Link to="/">
@@ -33,11 +26,6 @@ const Header = () => {
           buttonText={user.isAuthenticated ? "Logout" : "Login"}
           buttonStyle={"headerButton typo-sm"}
           onClick={() => (user.isAuthenticated ? logoutHandler() : handleLoginBtnClick())}
-        />
-        <Button
-          buttonText={"Playlist"}
-          buttonStyle={"headerButton typo-sm"}
-          onClick={() => showPlaylist()}
         />
       </div>
     </>
